526d66fc039cfa057260dccdebb4e32496662c30
Francisco Palop <francisco.palop@gmail.com>
2013-07-10 15:08:03 +0000

    Changed index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+#!/usr/bin/env node
+
 var express = require('express');
 
+var fs = require("fs");
 var app = express.createServer(express.logger());
+var content = fs.readFileSync("./index.html", "utf-8");
 
 app.get('/', function(request, response) {
-  response.send('Hello Francsico!');
+  response.send(buffer.toString("utf-8", content));
 });
 
 var port = process.env.PORT || 5000;
